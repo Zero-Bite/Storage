04e886a9aea20aacd3c70a1aafa5723733018a90
Gregory Goncharov <jacklamer@yandex.ru>
2023-10-28 00:56:35 +0300

    Extension

--- Data_structures/stack.py
+++ Data_structures/stack.py
@@ -1,30 +1,53 @@
 # задача на использование стека при решении задач со скобками
 
-line = " "
+def brackets_check(line):
 
-stack = []
-ex_sequence = '()'
-open_sequence = '('
-close_sequence = ')'
-first_commit_of_open = 0
+    stack = []
+    ex_sequence = '(){}[]'
+    open_sequence = '([{'
+    close_sequence = ')]}'
+    first_commit_of_open = 0
 
-for index in range(len(line)):
-    if line[index] in ex_sequence:
-        if line[index] in open_sequence:
+    for index in range(len(line)):
+        if line[index] in ex_sequence:
 
-            if line[index] == "(":
-                stack.append(line[index])
-                if len(stack) == 0:
-                    first_commit_of_open = index
+            if line[index] in open_sequence:
+                if line[index] == "(":
+                    stack.append(line[index])
+                    if len(stack) == 0:
+                        first_commit_of_open = index
+                if line[index] == "[":
+                    stack.append(line[index])
+                    if len(stack) == 0:
+                        first_commit_of_open = index
+                if line[index] == "{":
+                    stack.append(line[index])
+                    if len(stack) == 0:
+                        first_commit_of_open = index
                 
-        if line[index] in close_sequence:
-            if line[index] == ")":
-                if stack[-1] == '(':
-                    stack.pop()
-                else:
-                    print(index)
+                    
+            if line[index] in close_sequence:
+                if line[index] == ")":
+                    if len(stack) != 0 and stack[-1] == '(':
+                        stack.pop()
+                    else:
+                        return (index + 1)
+                if line[index] == "]":
+                    if len(stack) != 0 and stack[-1] == '[':
+                        stack.pop()
+                    else:
+                        return (index + 1)
+                if line[index] == "}":
+                    if len(stack) != 0 and stack[-1] == '{':
+                        stack.pop()
+                    else:
+                        return (index + 1)    
+            
 
-if len(stack) == 0:
-    print("Success")
-else:
-    print(first_commit_of_open)
+    if len(stack) == 0:
+        return "Success"
+    else:
+        return (first_commit_of_open + 1)
+    
+
+print(brackets_check("{[()]}"))
