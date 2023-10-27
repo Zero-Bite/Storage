17d61d09d3bbcf5c500a0879b82d1eaecc485b79
Gregory Goncharov <jacklamer@yandex.ru>
2023-10-28 00:30:52 +0300

    Structure attempt

--- second_parser.py
+++ second_parser.py
@@ -1,41 +1,37 @@
-# хотим спарсить поля с: 
-# javascript
-# cookie                         
-# User-agent
-
 import requests
 import fake_useragent
 import bs4
 
-# введем замену user-agent
-user = fake_useragent.UserAgent().random
-headers = {"user-agent": user,
-           "Sec-Ch-Ua-Platform": "macOS"}
+def second_step():
+    # введем замену user-agent
+    user = fake_useragent.UserAgent().random
+    headers = {"user-agent": user,
+            "Sec-Ch-Ua-Platform": "macOS"}
 
-link = "https://browser-info.ru/"
-response = requests.get(link, headers=headers)
+    link = "https://browser-info.ru/"
+    response = requests.get(link, headers=headers)
 
 
-if response.status_code == 200:
+    if response.status_code == 200:
 
-    print("<---Connection is ok--->")
+        print("<---Connection is ok--->")
 
-    all_data = {}
+        all_data = {}
 
-    soup = bs4.BeautifulSoup(response.text, 'lxml')
+        soup = bs4.BeautifulSoup(response.text, 'lxml')
 
-    # javascript state
-    js_state = soup.find('div', id = "javascript_check")
-    typ = js_state.text
-    all_data["JavaScript"] = typ
+        # javascript state
+        js_state = soup.find('div', id = "javascript_check")
+        typ = js_state.text
+        all_data["JavaScript"] = typ
 
-    # cookie
-    cookie_check = soup.find('div', id = "cookie_check").text
-    all_data["Cookie"] = cookie_check
+        # cookie
+        cookie_check = soup.find('div', id = "cookie_check").text
+        all_data["Cookie"] = cookie_check
 
-    #user-agent
-    user_info = soup.find('div', id = "user_agent").text
-    all_data["User-agent"] = user_info
+        #user-agent
+        user_info = soup.find('div', id = "user_agent").text
+        all_data["User-agent"] = user_info
 
-for element in all_data:
-    print(element)
+    return all_data
+
