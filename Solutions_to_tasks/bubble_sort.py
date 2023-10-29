0b2129d51fb62f8801c7adb0c7dade486e5365f0
Gregory Goncharov <jacklamer@yandex.ru>
2023-10-29 13:49:53 +0300

    Bubble

--- Solutions_to_tasks/bubble_sort.py
+++ Solutions_to_tasks/bubble_sort.py
@@ -1,4 +1,20 @@
+import random
 
 
-def bubble_sort(*args):
-    pass
+def bubble_sort(array):
+
+    for i in range(len(array) - 1):
+        for j in range(0, len(array) - 1 - i):
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+            
+    return array
+
+tmp_line = "2 14 15 21 24 28 30 35 36 42 50 54 55 58 59 74 77 79 91 95"
+line = [int(x) for x in tmp_line.split()]
+random.shuffle(line)
+
+print(line)
+
+result = bubble_sort(line)
+print(f"New sequence: {result}")
